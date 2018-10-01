1a19ce4c2a48d1b240829aabd8ff1c62a7551e06
Shane4666 <sc897@nau.edu>
2018-09-30 22:08:29 -0700

    path updated.

--- src/app/heroes/heroes-routes.module.ts
+++ src/app/heroes/heroes-routes.module.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 import { HeroesComponent } from './hero-summary-page/heroes.component';
 export const heroRoutes: Routes = [
     {
-        path: 'home',
-        component: HeroesComponent
-    },
-    {
       path: 'heroes',
       component: HeroesComponent
     }
